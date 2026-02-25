d9cd08fb39c5f5e6d970f79cd9dc30dde197af15
DevartDenisS <132042766+DevartDenisS@users.noreply.github.com>
2026-02-25 19:21:42 +0200

    Update DataQuery.sql

--- Data/DataQuery.sql
+++ Data/DataQuery.sql
@@ -1,28 +1,32 @@
-USE [SalesProdTS];
+USE [TeamCityProd];
 GO
 
-SET NOCOUNT ON;
+/* Delete in correct order because of FK */
+DELETE FROM dbo.Customer;
+DELETE FROM dbo.Vendor;
+DELETE FROM dbo.Person;
 GO
 
-/* =========================================================
-   DROP TABLES (DEPENDENCY ORDER)
-   ========================================================= */
-
-IF OBJECT_ID('[dbo].[Customer]', 'U') IS NOT NULL
-BEGIN
-    DROP TABLE [dbo].[Customer];
-END
+DBCC CHECKIDENT ('dbo.Person', RESEED, 0);
 GO
 
-IF OBJECT_ID('[dbo].[Vendor]', 'U') IS NOT NULL
-BEGIN
-    DROP TABLE [dbo].[Vendor];
-END
+INSERT INTO dbo.Person (FirstName, LastName, Title)
+VALUES
+(N'John',   N'Smith',    N'Mr'),
+(N'Emily',  N'Johnson',  N'Ms'),
+(N'Michael',N'Brown',    N'Mr');
 GO
 
-IF OBJECT_ID('[dbo].[Person]', 'U') IS NOT NULL
-BEGIN
-    DROP TABLE [dbo].[Person];
-END
+INSERT INTO dbo.Customer (PersonID, AccountNumber, Email)
+VALUES
+(1, N'CUST-1001', N'john.smith@gmail.com'),
+(2, N'CUST-1002', N'emily.johnson@yahoo.com'),
+(3, N'CUST-1003', N'michael.brown@outlook.com');
 GO
 
+INSERT INTO dbo.Vendor (PersonID, AccountNumber, Email, Phone)
+VALUES
+(1, N'VEND-2001', N'contact@smithsupplies.com',  N'+1-202-555-0147'),
+(2, N'VEND-2002', N'sales@johnsontrading.com',  N'+1-202-555-0193'),
+(3, N'VEND-2003', N'info@brownindustries.com',  N'+1-202-555-0118');
+GO
